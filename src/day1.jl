be330de1b32d6ecb79a30c7c92fcdfa6f66edf5f
Jameru <jaime.zb0@gmail.com>
2020-12-06 21:29:10 -0300

    Day 1.1

--- src/day1.jl
+++ src/day1.jl
@@ -6,16 +6,46 @@ function solve(arr)
 	pairs[idx][1] * pairs[idx][2]
 end
 
+function solve_for_2_better(input)
+	arr = sort(input)
+	lower_i = 1
+	upper_i = length(arr)
+	sum = arr[lower_i] + arr[upper_i]
+	while sum != 2020
+		if sum < 2020
+			lower_i += 1
+		else
+			upper_i -= 1
+		end
+		sum = arr[lower_i] + arr[upper_i]
+	end
+	arr[lower_i] * arr[upper_i]
+end
+
+function solve_for_2_also_better(input)
+	arr = input |> maximum |> falses
+	for x in input
+		arr[x] = true
+		if arr[2020 - x] == true
+			return x * (2020 - x)
+		end
+	end
+end
+
 function solve_for_n(arr, n)
 	pairs = collect(combinations(arr, n));
 	idx = findfirst(x -> sum(x) == 2020, pairs);
 	reduce(*, pairs[idx])
 end
 
-test_input = [1721 979 366 299 675 1456];
+test_input = [1721, 979, 366, 299, 675, 1456];
 @assert solve(test_input) == 514579 ["test case 1 failed"];
+@assert solve_for_2_better(test_input) == 514579 ["test case 1a failed"];
+@assert solve_for_2_also_better(test_input) == 514579 ["test case 1b failed"];
 @assert solve_for_n(test_input, 3) == 241861950 ["test case 2 failed"];
 
 input = map(x -> parse(Int64, x), readlines("input/day1.txt"));
-println("First star: " * string(solve(input)))
-println("Second star: " * string(solve_for_n(input, 3)))
+println("First star: " * string(@time solve(input)))
+println("First star: " * string(@time solve_for_2_better(input)))
+println("First star: " * string(@time solve_for_2_also_better(input)))
+println("Second star: " * string(@time solve_for_n(input, 3)))
